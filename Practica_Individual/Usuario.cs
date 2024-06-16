d40d2971bb127efd1bffc07f3b9c23908a9b5c31
P.Rodriguez@alumni. ufv <150132801+070704@users.noreply.github.com>
2024-06-16 22:10:28 +0200

    cambiod

--- Practica_Individual/Usuario.cs
+++ Practica_Individual/Usuario.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,6 +9,8 @@ namespace Practica_Individual
 {
     internal abstract  class Usuario 
     {
+        private int[,] rodillos;
+        private Random random;
         protected static List <Premio> Premios = new List <Premio> ();
         public Usuario() { }
 
@@ -20,14 +23,39 @@ namespace Practica_Individual
 
         public void JugarTraga()
         {
+            rodillos = new int[3,3];
+            random = new Random();
+
+            for(int i = 0;i < 3; i++)
+            {
+                for(int j = 0;j < 3;j++)
+                {
+                    rodillos[i,j] = random.Next(1,7);
+                    Console.WriteLine($"{rodillos[i,j]}");
+                }
+
+            }
+           
+
 
         }
 
         public void MostrarInfo()
         {
-
+            foreach (var premio in Premios)
+            {
+                if (rodillos[1, 0] == premio.Simbolo1 && rodillos[1, 1] == premio.Simbolo2 && rodillos[1, 2] == premio.Simbolo3)
+                {
+                    Console.WriteLine($"¡Ganaste! Premio: {premio.Nombre}");
+                    
+                    return;
+                }
+            }
+            Console.WriteLine("No has ganado esta vez.");
         }
+    }
         
         
     }
-}
+ 
+
